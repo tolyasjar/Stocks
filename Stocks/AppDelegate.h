fc72ccc4e610dc8ecde49bcaf13110cd5f2d4f65
tolyasjar <toleenj@gmail.com>
2016-07-05 11:35:44 -0500

    Initial Commit

---
+++ Stocks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Stocks
+//
+//  Created by Toleen Jaradat on 7/5/16.
+//  Copyright © 2016 Toleen Jaradat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Stocks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Stocks
+//
+//  Created by Toleen Jaradat on 7/5/16.
+//  Copyright © 2016 Toleen Jaradat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
